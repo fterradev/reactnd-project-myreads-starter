a44543597c8a62fe2aa75f4b6bf22c4128f8fbe8
Fernando Terra <nandobrt@gmail.com>
2017-09-08 23:33:19 -0300

    feat: Add routes that decide which UI to render

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React from 'react'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
 import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
-    
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
   }
 
   componentDidMount() {
@@ -34,15 +27,15 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks onNavigateToList={() => this.setState({ showSearchPage: false })} />
-        ) : (
+        <Route path='/search' render={() => (
+          <SearchBooks />
+        )}/>
+        <Route exact path='/' render={() => (
           <ListBooks
             books={this.state.books}
-            onNavigateToSearch={() => this.setState({ showSearchPage: true })}
             onMoveBook={this.onMoveBook}
           />
-        )}
+        )}/>
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import BookShelf from './BookShelf'
+import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
   render() {
@@ -37,7 +38,7 @@ class ListBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={this.props.onNavigateToSearch}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
