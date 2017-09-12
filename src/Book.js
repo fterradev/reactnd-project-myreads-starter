f69555af24218b092438413b7db5a72f957ef8bf
Fernando Terra <nandobrt@gmail.com>
2017-09-12 13:31:45 -0300

    fix: Split authors into multiple elements
    Authors' names were concatenaded without separation as the text content of a single HTML element.
    
    This has lead to a new Book component.

---
+++ src/Book.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+  render() {
+    const { data, onMoveBook } = this.props
+    const { authors = [] } = data
+    return (
+      <li key={data.id}>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: '100%',
+                height: '100%',
+                backgroundImage: data.imageLinks ? `url("${data.imageLinks.thumbnail}")` : ''
+              }}
+            ></div>
+            <div className="book-shelf-changer">
+              <select value={data.shelf} onChange={(event) => onMoveBook(data, event.target.value)}>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{data.title}</div>
+          {authors.map((author, index) => (
+            <div key={index} className="book-authors">{author}</div>
+          ))}
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Book
