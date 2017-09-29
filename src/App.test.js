43d8e069aca5d901669e34a50bc3e80748be74f5
Fernando Terra <nandobrt@gmail.com>
2017-09-29 15:53:14 -0300

    test: Add basic render test to App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 /** 
  This course is not designed to teach Test Driven Development. 
@@ -10,5 +11,10 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+    div
+  );
 });
