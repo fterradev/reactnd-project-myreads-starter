caaf463e6ef1686e1bc86246979b4cea83be4f17
Fernando Terra <nandobrt@gmail.com>
2017-10-02 13:18:47 -0300

    refactor: Turn component into PureComponent

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Book from './Book';
 import PropTypes from 'prop-types';
 
-const BooksGrid = ({ shelfId, books, onMoveBook }) => {
-  const gridBooks = (shelfId !== undefined)
-    ? books.filter(book => book.shelf === shelfId)
-    : books;
-  return (
-    <ol className="books-grid">
-      {gridBooks.map(book => 
-        <Book key={book.id} data={book} onMoveBook={onMoveBook} />
-      )}
-    </ol>
-  );
-};
-
-BooksGrid.propTypes = {
-  shelfId: PropTypes.oneOf(['wantToRead', 'currentlyReading', 'read']),
-  books: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onMoveBook: PropTypes.func.isRequired
-};
+class BooksGrid extends PureComponent {//= ({ shelfId, books, onMoveBook }) => {
+  static propTypes = {
+    shelfId: PropTypes.oneOf(['wantToRead', 'currentlyReading', 'read']),
+    books: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onMoveBook: PropTypes.func.isRequired
+  };
+  render() {
+    const { shelfId, books, onMoveBook } = this.props;
+    const gridBooks = (shelfId !== undefined)
+      ? books.filter(book => book.shelf === shelfId)
+      : books;
+    return (
+      <ol className="books-grid">
+        {gridBooks.map(book => 
+          <Book key={book.id} data={book} onMoveBook={onMoveBook} />
+        )}
+      </ol>
+    );
+  }
+}
 
 export default BooksGrid;
