160a637c0b9a8de6349505a4a0249df2c8f25bf8
Fernando Terra <nandobrt@gmail.com>
2017-10-02 13:15:13 -0300

    refactor: Turn component into PureComponent

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import BooksGrid from './BooksGrid';
 import PropTypes from 'prop-types';
 
-const Bookshelf = ({ id, title, books, onMoveBook }) => (
-  <div className="bookshelf">
-    <h2 className="bookshelf-title">{title}</h2>
-    <div className="bookshelf-books">
-      <BooksGrid shelfId={id} books={books} onMoveBook={onMoveBook} />
-    </div>
-  </div>
-);
+class Bookshelf extends PureComponent {
+  static propTypes = {
+    id: PropTypes.oneOf(['wantToRead', 'currentlyReading', 'read']).isRequired,
+    title: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onMoveBook: PropTypes.func.isRequired
+  };
 
-Bookshelf.propTypes = {
-  id: PropTypes.oneOf(['wantToRead', 'currentlyReading', 'read']).isRequired,
-  title: PropTypes.string.isRequired,
-  books: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onMoveBook: PropTypes.func.isRequired
-};
+  render() {
+    const { id, title, books, onMoveBook } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <BooksGrid shelfId={id} books={books} onMoveBook={onMoveBook} />
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default Bookshelf;
