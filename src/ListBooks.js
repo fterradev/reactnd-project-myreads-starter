e97db92e0246b575f3920af6e04f7d805f8b5981
Fernando Terra <nandobrt@gmail.com>
2017-10-02 13:12:05 -0300

    refactor: Turn component into PureComponent

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Bookshelf from './Bookshelf';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ListBooks = ({ shelves, books, onMoveBook }) => {
-  return (
-    <div className="list-books">
-      <div className="list-books-title">
-        <h1>MyReads</h1>
-      </div>
+class ListBooks extends PureComponent {
+  static propTypes = {
+    shelves: PropTypes.arrayOf(PropTypes.object).isRequired,
+    books: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onMoveBook: PropTypes.func.isRequired
+  };
 
-      <div className="list-books-content">
-        <div>
-          {shelves.map(shelf => (
-            <Bookshelf
-              key={shelf.id}
-              title={shelf.title}
-              id={shelf.id}
-              books={books}
-              onMoveBook={onMoveBook}
-            />
-          ))}
+  render() {
+    const { shelves, books, onMoveBook } = this.props;
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+
+        <div className="list-books-content">
+          <div>
+            {shelves.map(shelf => (
+              <Bookshelf
+                key={shelf.id}
+                title={shelf.title}
+                id={shelf.id}
+                books={books}
+                onMoveBook={onMoveBook}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
-      
-      <div className="open-search">
-        <Link to="/search">Add a book</Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
 
-ListBooks.propTypes = {
-  shelves: PropTypes.arrayOf(PropTypes.object).isRequired,
-  books: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onMoveBook: PropTypes.func.isRequired
-};
-
+}
 export default ListBooks;
